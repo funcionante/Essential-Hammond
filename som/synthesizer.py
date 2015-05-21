dc934bca5ec56d3ded93ba44a9417958f89a37c9
Leonardo Oliveira <leonardooliveira@ua.pt>
2015-05-21 19:23:42 +0100

    Adicionada primeira versão do sintetizador

---
+++ som/synthesizer.py
@@ -0,0 +1,62 @@
+import json
+import math
+from math import sin, pi
+
+def synthesizer(filename, registration):
+	infile = open(filename, 'r')
+	content = infile.read()
+
+	#json from interpreter with the info of notes and duration. pairs frequence-time
+	pairs = json.loads(content)
+
+	#dictio to be converted into json with all the samples of the music. pairs frequence-samples
+	sounds = []
+	
+	#frequences of the 9 oscillators calculated for each note
+	freqs = [None] * 9
+	
+	#ratio to calculate frequences (lookup table)
+	ratio = [1.0/2, 2.0/3, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 8.0]
+	
+	#amplitudes calculated for each note by the registration
+	amplitudes = [None] * 9
+	
+	rate = 44100
+	
+	for pair in pairs:
+		mainfreq = pair['freq']
+		duration = pair['time']
+		
+		for i in range(0, 9):
+			freqs[i] = mainfreq * ratio[i]
+			amplitudes[i] = float(registration[i]) / 8 * 2000
+		
+		data = []
+		
+		
+		nsamples = int(rate * duration)
+			
+		for i in range(0, nsamples):
+			
+			sample = 0
+			
+			for j in range(0, 9):
+				sample += amplitudes[j] * sin(2 * math.pi * freqs[j] * i / rate)
+			data.append(sample)
+
+		print len(data)
+		
+		sounds2 = {}
+		
+		sounds.append({'freq': mainfreq, 'samples': data})
+		
+		
+		print sounds
+		
+	outfile = open('2.json', 'w')
+	json.dump(sounds, outfile)
+	outfile.close()
+
+if __name__ == '__main__':
+	
+	synthesizer('1.json', "008000000")
