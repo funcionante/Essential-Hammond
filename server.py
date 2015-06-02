fd093e6217e6df7566578e1521bed039b983d95b
Dennis <bartashevich@ua.tp>
2015-06-02 17:31:10 +0100

    Changed way to get parameters from the url

--- server.py
+++ server.py
@@ -16,6 +16,14 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 class Root(object):
 
 	@cherrypy.expose
+	def addVote(self, id=None):
+		try:
+			add_upvotes(id)
+
+		except Exception, e:
+			return "Something went wrong"
+
+	'''
 	def addVote(self, **kw):
 		try:
 			x = repr(dict(kw=kw))
@@ -25,9 +33,17 @@ class Root(object):
 			else:
 				return "Error"
 		except Exception, e:
-			return "Something went wrong"
+			return "Something went wrong"'''
 
 	@cherrypy.expose
+	def delVote(self, id=None):
+		try:
+			add_downvotes(id)
+
+		except Exception, e:
+			return "Something went wrong"
+
+	'''
 	def delVote(self, **kw):
 		try:
 			x = repr(dict(kw=kw))
@@ -37,16 +53,29 @@ class Root(object):
 			else:
 				return "Error"
 		except Exception, e:
-			return "Something went wrong"
+			return "Something went wrong"'''
 
 	#DONE!
 	@cherrypy.expose
 	def index(self):
 		return open("index.html","r")
 
+	@cherrypy.expose
+	def createSong(self, name=None, notes=None):
+		try:
+			name = name.decode("utf-8")
+			notes = notes.decode("utf-8")
+			create_song(name, notes)
+
+			create_image(generate_pairs(name+':'+notes),'img/'+str(last_id())+'.jpg')
+			return "Musica enviada com sucesso!"
+
+		except Exception, e:
+			return "Something went wrong"
+
 	#http://localhost:8080/createSong?name=The%20Simpsons&notes=c.6,e6,f%236,8a6,g.6,e6,c6,8a,8f%23,8f%23,8f%23,2g,8p,8p,8f%23,8f%23,8f%23,8g,a%23.,8c6,8c6,8c6,c6
 	#DONE!
-	@cherrypy.expose
+	'''
 	def createSong(self, **kw):
 		try:
 			x = repr(dict(kw=kw))
@@ -59,12 +88,25 @@ class Root(object):
 			return "Música enviada com sucesso!"
 
 		except Exception, e:
-			return "Error"
+			return "Error"'''
+
+	@cherrypy.expose
+	def createInterpretation(self, registration = None, id = None, effects = None, name = None):
+		try:
+			create_interpretation(registration,effects,id,name)
+			getno = get_notes_and_name(id)
+			pauta = getno[0] + ":" + getno[1]
+			filelocation = 'audio/'+str(last_id_interpretations())+'.wav'
+
+			create_wav_file(filelocation, generate_sounds(generate_pairs(pauta), registration), effects)
+
+			return "Interpretação enviada com sucesso!"
+		except Exception, e:
+			return "Something went wrong"
 
 	#http://localhost:2223/createInterpretation?registration=888888888&id=40&effects=none&name=LOL
 	#DONE!
-	@cherrypy.expose
-	def createInterpretation(self, **kw):
+	'''def createInterpretation(self, **kw):
 		try:
 			x = repr(dict(kw=kw))
 			x = x.split("'")
@@ -80,10 +122,19 @@ class Root(object):
 				create_wav_file(filelocation, generate_sounds(generate_pairs(pauta), regist), effect)
 			return "Interpretação enviada com sucesso!"
 		except Exception, e:
-			return "Error"
+			return "Error"'''
+
+	@cherrypy.expose
+	def getWaveForm(self, id=None):
+		try:
+			x = "img/"+id+".jpg"
+
+		except Exception, e:
+			return "Something went wrong"
+		raise cherrypy.HTTPRedirect(x)
 
 	#DONE!
-	@cherrypy.expose
+	'''
 	def getWaveForm(self, **kw):
 		try:
 			x = repr(dict(kw=kw))
@@ -94,10 +145,19 @@ class Root(object):
 				return "Error"
 		except Exception, e:
 			return "Something went wrong"
+		raise cherrypy.HTTPRedirect(x)'''
+
+	@cherrypy.expose
+	def getWaveFile(self, id=None):
+		try:
+			x = "audio/"+id+".wav"
+
+		except Exception, e:
+			return "Something went wrong"
 		raise cherrypy.HTTPRedirect(x)
 
 	#DONE! FALTA CHECK IF EXIST
-	@cherrypy.expose
+	'''
 	def getWaveFile(self, **kw):
 		try:
 			x = repr(dict(kw=kw))
@@ -108,10 +168,18 @@ class Root(object):
 				return "Error"
 		except Exception, e:
 			return "Something went wrong"
-		raise cherrypy.HTTPRedirect(x)
+		raise cherrypy.HTTPRedirect(x)'''
+
+	@cherrypy.expose
+	def getNotes(self, id=None):
+		try:
+			return get_notes(id)
+
+		except Exception, e:
+			return "Something went wrong"
 
 	#DONE! FALTA CHECK IF EXIST
-	@cherrypy.expose
+	'''
 	def getNotes(self, **kw):
 		try:
 			x = repr(dict(kw=kw))
@@ -124,9 +192,18 @@ class Root(object):
 			else:
 				return "ERROR"
 		except Exception, e:
-			return "Something went wrong"
+			return "Something went wrong"'''
 
 	@cherrypy.expose
+	@cherrypy.tools.json_out()
+	def listSongFiles(self, id=None):
+		try:
+			return get_songs(id)
+
+		except Exception, e:
+			return "Something went wrong"
+
+	'''
 	@cherrypy.tools.json_out()
 	def listSongFiles(self, **kw):
 		try:
@@ -140,25 +217,43 @@ class Root(object):
 			else:
 				return "ERROR"
 		except Exception, e:
-			return "Something went wrong"
+			return "Something went wrong"'''
 
-	#DONE!
 	@cherrypy.expose
 	@cherrypy.tools.json_out()
 	def listNotes(self):
-		return get_all_notes()
+		try:
+			return get_all_notes()
+
+		except Exception, e:
+			return "Something went wrong"
 
 	#DONE!
+	'''
+	@cherrypy.tools.json_out()
+	def listNotes(self):
+		return get_all_notes()'''
+
 	@cherrypy.expose
 	@cherrypy.tools.json_out()
 	def listSongs(self):
-		return get_all_songs()
+		try:
+			return get_all_songs()
+
+		except Exception, e:
+			return "Something went wrong"
+
+	#DONE!
+	'''
+	@cherrypy.tools.json_out()
+	def listSongs(self):
+		return get_all_songs()'''
 
 	#DONE! JUST FOR TEST
 	@cherrypy.expose
 	def allmusic(self):
 		return open("allmusic.html","r")
- 
+
 	#JUST FOR TEST
 	@cherrypy.expose
 	def upload(self):
