b2357ee7d4b180071435621d80c07c923b5d3fb4
Leonardo Oliveira <leonardooliveira@ua.pt>
2015-06-04 00:31:23 +0100

    Adicionada páginas web da versão desktop.

---
+++ desk/js/scripts.js
@@ -0,0 +1,84 @@
+$(function(){
+    //function to send a song to the server
+    $('#send-song').on('click', function(){
+
+        var $code = $('#code');
+        var $alerts = $('#alerts');
+        $alerts.empty(); //removes any previous allerts
+
+        splited = $code.val().split(":");
+        var name = splited[0];
+        var notes = splited[1]+":"+splited[2];
+
+        var song = {
+            name: name,
+            notes: notes,
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: 'createSong',
+            data: song,
+            success: function(){
+                $alerts.append('<div class="alert alert-success pull-right" role="alert">Música enviada com sucesso!</div>'); //add a succss alert
+                $code.val(""); //cleans the input area
+            }
+        })
+    });
+
+    //function to send a interpretation to the server
+    $('#send-interpretation').on('click', function(){
+
+        var $name = $('#name');
+        var $songs = $('#noteslist');
+        var $effects = $('#efeitos');
+        var $registration = $('#registration');
+        var $alerts = $('#alerts');
+        $alerts.empty(); //removes any previous allerts
+
+        console.log($songs);
+        console.log($effects);
+        console.log($songs.find(":selected")[0]);
+        console.log($effects.find(":selected")[0]);
+        console.log($songs.find(":selected").attr("value"));
+        console.log($effects.find(":selected").attr("value"));
+
+        var interpretation = {
+            name: $name.val(),
+            id: $songs.find(":selected").attr("value"),
+            effects: $effects.find(":selected").attr("value"),
+            registration: $registration.val(),
+        };
+
+        $.ajax({
+            type: 'POST',
+            url: 'createInterpretation',
+            data: interpretation,
+            success: function(){
+                $alerts.append('<div class="alert alert-success pull-right" role="alert">Interpretação enviada com sucesso!</div>'); //add a succss alert
+                $name.val(""); //cleans the input area
+                $registration.val("");
+            }
+        })
+    });
+
+
+    //function to get the list of musics
+    $.ajax({
+        type: 'GET',
+        url: '/listNotes',
+        success: function(data) {
+
+            var $list = $('#noteslist');
+
+            for (var i = 0; i < data.length; i++) {
+                $list.append('<option value="'+data[i].id+'">'+data[i].name+'</option>');
+            }
+
+        }
+
+    });
+    
+
+    
+});
