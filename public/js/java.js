c0b67ae218b9cce440d3e044125e3e82d58aca93
Domingos Nunes <dfsn@ua.pt>
2015-06-04 17:30:26 +0100

    JS da Intepretação quase concluida

--- public/js/java.js
+++ public/js/java.js
@@ -1,34 +1,21 @@
 function refreshJSON () {
     location.reload();
-
-    $.ajax({
-	type: 'GET',
-	url: '/listNotes',
-	success: function(notes){
-		var $notesInt = $('#notesInt');
-		//var $notesPlay = $('#notesPlay');
-		x = notes[0].name;
-		for (var i = 0; i < notes.length; i++) {
-			$notesInt.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
-			//$notesPlay.append('<option value="'+i+'">'+notes[i].name+'</option>');
-		};
-	}
-});
+};
 
 $.ajax({
 	type: 'GET',
 	url: '/listSongs',
 	success: function(notes){
-		//var $notesInt = $('#notesInt');
+		var $notesInt = $('#notesInt');
 		var $notesPlay = $('#notesPlay');
 		x = notes[0].name;
 		for (var i = 0; i < notes.length; i++) {
-			//$notesInt.append('<option value="'+i+'">'+notes[i].name+'</option>');
+			$notesInt.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
 			$notesPlay.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
 		};
 	}
 });
-}
+
 
 
 $.ajax({
@@ -36,25 +23,11 @@ $.ajax({
 	url: '/listNotes',
 	success: function(notes){
 		var $notesInt = $('#notesInt');
-		//var $notesPlay = $('#notesPlay');
-		x = notes[0].name;
-		for (var i = 0; i < notes.length; i++) {
-			$notesInt.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
-			//$notesPlay.append('<option value="'+i+'">'+notes[i].name+'</option>');
-		};
-	}
-});
-
-$.ajax({
-	type: 'GET',
-	url: '/listNotes',
-	success: function(notes){
-		//var $notesInt = $('#notesInt');
 		var $notesPlay = $('#notesPlay');
 		x = notes[0].name;
 		for (var i = 0; i < notes.length; i++) {
-			//$notesInt.append('<option value="'+i+'">'+notes[i].name+'</option>');
-			$notesPlay.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
+			$notesInt.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
+			$notesPlay.append('<option value="'+i+'">'+notes[i].name+'</option>');
 		};
 	}
 });
@@ -68,7 +41,9 @@ function addNote() {
 	var name = all[0];
 	var notas = all[1]+":"+all[2];
 	var x = "name="+encodeURIComponent(name)+"&notes="+encodeURIComponent(notas);
+
 	$.post("createSong",x,function(response){
+		console.log(response);
 		if(response == "Sucess"){
 			document.getElementById("himynameis").innerHTML = "Sucess!";
 		}
@@ -76,4 +51,13 @@ function addNote() {
 			document.getElementById("himynameis").innerHTML = "Error!";
 		}
 	});
+}
+
+function addMusic () {
+	var id = document.getElementById("notesInt").selectedIndex;
+	var registo = document.getElementById("registo");
+	var efeito = document.getElementById("efeito").selectedIndex;
+	var nome = document.getElementById("nome");
+	var x = "registration="+registo+"&id="+id+"&effects="+efeito+"&name=+"+nome;
+
 }
