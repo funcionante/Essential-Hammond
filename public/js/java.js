e08141f0a6062008004a42b30d8ffc7d9ab88f64
Domingos Nunes <dfsn@ua.pt>
2015-06-05 21:53:32 +0100

    App em contrução

--- public/js/java.js
+++ public/js/java.js
@@ -4,11 +4,10 @@ function refreshJSON () {
 
 $.ajax({
 	type: 'GET',
-	url: '/listSongs',
+	url: '/listNotes',
 	success: function(notes){
 		var $notesInt = $('#notesInt');
 		var $notesPlay = $('#notesPlay');
-		x = notes[0].name;
 		for (var i = 0; i < notes.length; i++) {
 			$notesInt.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
 			$notesPlay.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
@@ -16,22 +15,21 @@ $.ajax({
 	}
 });
 
+function MusicMenu () {
+	id = document.getElementById("notesPlay").value;
+	url1 = 'listSongFiles?id='+id;
 
-
-$.ajax({
-	type: 'GET',
-	url: '/listNotes',
-	success: function(notes){
-		var $notesInt = $('#notesInt');
-		var $notesPlay = $('#notesPlay');
-		x = notes[0].name;
-		for (var i = 0; i < notes.length; i++) {
-			$notesInt.append('<option value="'+notes[i].id+'">'+notes[i].name+'</option>');
-			$notesPlay.append('<option value="'+i+'">'+notes[i].name+'</option>');
-		};
-	}
-});
-
+	$.ajax({
+		type: 'GET',
+		url: url1,
+		success: function(songs){
+			var $songs= $('#songs');
+			for (var i = 0; i < songs.length; i++) {
+				$songs.append('<div data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u"><h4>'+songs[i].name+'</h4><audio controls><source src="http://localhost:8080/audio/1.wav" type="audio/wav"></audio></div>');
+			};
+		}
+	});
+}
 
 function addNote() {
 	var elem = document.getElementById("text");
@@ -59,6 +57,8 @@ function addMusic () {
 	var efeito = document.getElementById("efeito").value;
 	var nome = document.getElementById("nome").value;
 	var x = "registration="+registo+"&id="+id+"&effects="+efeito+"&name=+"+nome;
+
+	console.log(x);
 
 	$.post("createInterpretation",x,function(response){
 		if(response == "Sucess"){
