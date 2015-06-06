5333a8453687e660f692aebee4aca2b4062b11f3
Leonardo Oliveira <leonardooliveira@ua.pt>
2015-06-06 20:44:47 +0100

    Ficheiros de teste alterados.

--- som/decreasing_list_test.py
+++ som/decreasing_list_test.py
@@ -3,12 +3,17 @@ from effects_processor import get_decreasing
 #this program gives a graphical view of the decreasing list
 
 dec = get_decreasing()
+k = 0
 
 for i in dec:
 
-		line = ''
+	line = ''
+	
+	if k%8 == 0:
 		
 		for j in range (0, int(i*50)):
 			line += '*'
-		
-		print line
+			
+		print ('%3d')%(k) + ' ' + line
+	
+	k+=1
--- som/envelope_list_test.py
+++ som/envelope_list_test.py
@@ -3,12 +3,17 @@ from effects_processor import get_env
 #this program gives a graphical view of the envelope list
 
 env = get_env()
+k = 0
 
 for i in env:
 
-		line = ''
-		
+	line = ''
+	
+	if k%2 == 0:
+	
 		for j in range (0, int(i*50)):
 			line += '*'
 		
-		print line
+		print ('%3d')%(k) + ' ' + line
+		
+	k+=1
