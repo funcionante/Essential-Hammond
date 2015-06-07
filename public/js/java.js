1db5d3dd2618eb1a056e7f9684979dfd7a44208c
Domingos Nunes <dfsn@ua.pt>
2015-06-07 05:12:40 +0100

    App Concluída

--- public/js/java.js
+++ public/js/java.js
@@ -15,15 +15,30 @@ $.ajax({
 	}
 });
 
+function upVote (id) {
+	var x = "id="+id;
+	$.post("addVote",x,function(response){
+	});
+
+	alert("Gosto enviado com sucesso!");
+}
+
+function downVote (id) {
+	var x = "id="+id;
+	$.post("delVote",x,function(response){
+	});
+
+	alert("Não gosto enviado com sucesso!");
+}
+
 function MusicMenu () {
 	music = ""+document.getElementById("notesPlay").value;
 	for (var i = 0; i < id.length; i++) {
 		if (id[i].charAt(1) == music) {
 			$('#'+id[i]).slideDown();
+			$('#PlaySubmit').hide();
 		};
 	};
-
-	$('#playSubmit').hide();
 }
 
 $.ajax({
@@ -33,7 +48,7 @@ $.ajax({
 		var $songs= $('#songs');
 		id = ["test"];
 		for (var i = 0; i < songs.length; i++) {
-			$songs.append('<div id="'+songs[i].id+''+songs[i].id_music+'" data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u"><h4>'+songs[i].name+'</h4><audio controls><source src="http://localhost:8080/audio/'+songs[i].id+'.wav" type="audio/wav"></audio></div>');
+			$songs.append('<div id="'+songs[i].id+''+songs[i].id_music+'" data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" style="text-align: center"><div class="ui-grid-solo"><div class="ui-block-a"><h4>'+songs[i].upvotes+' gosto(s) e '+songs[i].downvotes+' não gosto(s)</h4></div></div><div class="ui-grid-a" style="text-align: center"><div class="ui-block-a"><input type="button" data-icon="star" value="Gosto" onclick="upVote('+songs[i].id+')"></div><div class="ui-block-b"><input type="button" onclick="downVote('+songs[i].id+')"data-icon="delete" value="Não gosto"></div></div><div class="ui-grid-solo"><div class="ui-block-a"><a data-rel="popup" href="#img'+songs[i].id_music+'" class="ui-btn ui-shadow ui-corner-all">Imagem</a></div><div id="img'+songs[i].id_music+'" data-role="popup" data-overlay-theme="a" class="ui-content"><img style="width:300px" src="/img/'+songs[i].id_music+'.jpg"></img></div></div><h4>'+songs[i].name+'</h4><audio controls><source src="http://localhost:8080/audio/'+songs[i].id+'.wav" type="audio/wav"></audio></div>');
 			id.push(songs[i].id+''+songs[i].id_music);
 		};
 
